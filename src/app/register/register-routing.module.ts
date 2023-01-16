4b835e4b0aa9f66f396bbdc2d4af45a9e9516b03
Federico González <federico1818@gmail.com>
2023-01-16 13:04:20 -0300

    create route app file

--- src/app/register/register-routing.module.ts
+++ src/app/register/register-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
+import { APP_ROUTES } from 'src/app/app-routes'
+
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'register',
+        redirectTo: APP_ROUTES.REGISTER,
         pathMatch: 'full'
     }
 ]
