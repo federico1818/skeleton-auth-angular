ab00a51be62b0e53c2c8bf151bafbf5423c61efc
Federico González <federico1818@gmail.com>
2023-01-16 13:25:49 -0300

    create register form

--- src/app/register/components/register-form/register-form.component.ts
+++ src/app/register/components/register-form/register-form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
-  selector: 'app-register-form',
-  templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+    selector: 'app-register-form',
+    templateUrl: './register-form.component.html',
+    styleUrls: ['./register-form.component.scss']
 })
-export class RegisterFormComponent {
 
+export class RegisterFormComponent {
+    constructor(
+        private fb: FormBuilder
+    ) {}
+
+    public form: FormGroup = this.fb.group({
+        name: ['', Validators.required]
+    })
+
+    public onSubmit(): void {
+        if(this.form.valid)
+            this.send()
+    }
+
+    protected send(): void {
+        console.log(this.form.value)
+    }
 }
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { RegisterRoutingModule } from './register-routing.module'
 import { RegisterComponent } from './pages/register/register.component';
@@ -13,6 +14,8 @@ import { RegisterFormComponent } from './components/register-form/register-form.
     ],
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         RegisterRoutingModule
     ]
 })
