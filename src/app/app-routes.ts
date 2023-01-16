87b6ba75209eb8aa3db8343bd37f8a250f33246c
Federico González <federico1818@gmail.com>
2023-01-16 13:08:54 -0300

    add register form

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,3 +1,3 @@
 export const APP_ROUTES = {
     REGISTER: 'register',
-}
+} as const
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { RegisterRoutingModule } from './register-routing.module'
-import { RegisterComponent } from './pages/register/register.component'
+import { RegisterComponent } from './pages/register/register.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component'
 
 
 @NgModule({
     declarations: [
-        RegisterComponent
+        RegisterComponent,
+        RegisterFormComponent
     ],
     imports: [
         CommonModule,
