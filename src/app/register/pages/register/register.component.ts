0efb22507f6540a4cbbffd9dfe1b335814b76c78
Federico González <federico1818@gmail.com>
2023-01-18 13:51:00 -0300

    pass user to register success component

--- src/app/register/pages/register/register.component.ts
+++ src/app/register/pages/register/register.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { RegisterResponseDTO } from 'src/app/register/register-response-dto'
+import { RegisterFormService } from 'src/app/register/services/register-form.service'
+import { NavigationService } from 'src/app/shared/services/navigation.service'
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+
+export class RegisterComponent implements OnInit {
+    constructor(
+        private registerFormService: RegisterFormService,
+        private navigationService: NavigationService
+    ) {}
+
+    public ngOnInit(): void {
+        this.registerFormService.onSuccess.subscribe((res: RegisterResponseDTO) => {
+            this.navigationService.goToRegisterSuccess(res.data)
+        })
+    }
 
 }
